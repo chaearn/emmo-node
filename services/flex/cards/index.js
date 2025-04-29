7c66895f70de8f9a3db5cc2b8b3486da74885064
chaearn <chaearn@gmail.com>
2025-04-30 03:42:00 +0700

    Update index.js

--- services/flex/cards/index.js
+++ services/flex/cards/index.js
@@ -1,9 +1,11 @@
 const imageCTA = require('./imageCTA');
 const progressCard = require('./progressCard');
 const quoteCard = require('./quoteCard');
+const tipsCard = require('./tipsCard');
 
 module.exports = {
   imageCTA,
   progressCard,
-  quoteCard
+  quoteCard,
+  tipsCard
 };
